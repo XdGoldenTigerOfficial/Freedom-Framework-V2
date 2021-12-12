eae86d832483ecc40395dc17cd1bd806a38ac2a1
Xd_Golden_Tiger <68713969+XdGoldenTigerOfficial@users.noreply.github.com>
2021-12-12 23:21:17 +1300

    Uploaded Client Files
    Fixed a lot of the build errors. Updated ScripthookVnet and CitizenFX

---
+++ FreedomFramework.Client/CEO/CEOClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using CitizenFX.Core.UI;
+
+namespace client.CEO
+{
+    public class CEOClient : BaseScript
+    {
+        public static bool CanRegisterCEO = false;
+        public CEOClient()
+        {
+            API.RegisterCommand("organization", new Action<int, List<object>, string>((src, args, raw) =>
+            {
+                var argList = args.Select(o => o.ToString()).ToList();
+
+                if (argList.Any())
+                {
+                    string displayString = string.Join(" ", argList.ToArray());
+
+                    TriggerServerEvent("Freedom:CreateOrganization", displayString);
+                }
+            }), false);
+        }
+    }
+}
